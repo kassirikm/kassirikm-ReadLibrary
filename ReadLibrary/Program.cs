f6c5ec785be416c37ef4a7a8b205b47aa482b379
kassirikm <kassirikm@hotmail.fr>
2022-03-22 15:36:45 +0100

    Version 1

--- ReadLibrary/Program.cs
+++ ReadLibrary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ReadLibrary
 {
@@ -6,7 +7,43 @@ namespace ReadLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wat wil je doen?");
+            Console.WriteLine("1) Lees Text File.");
+            var keuze = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Wat is de naam van je document?");
+            var input = Console.ReadLine();
+
+            switch (keuze)
+            {
+                case 1:
+                    ReadText(input);//textFile
+                    Console.WriteLine("Uw toegangsniveau geeft je geen toegang tot deze file.");
+                    break;
+                default:
+                    Console.WriteLine("Dit keuze bestaat niet.");
+                    break;
+            }
+        }
+
+        static void ReadText(string input)
+        {
+
+            try
+            {
+                // Open de text file met stream reader.
+                using (var sr = new StreamReader(input + ".txt"))
+                {
+                    // Lees de stream als een string, en schrijf de string naar de console.
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("De file kon niet gelezen worden:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
