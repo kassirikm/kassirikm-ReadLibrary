97e591df770e572c0a279b4d05aefbbf6583805c
kassirikm <kassirikm@hotmail.fr>
2022-03-22 15:31:25 +0100

    Add project files.

---
+++ ReadLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
